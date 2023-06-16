7644478168e05767f1bb82cadabf922ac3ab817a
samresh9 <thapasamresh9@gmail.com>
2023-06-16 20:59:38 +0545

    PT-23: Improve the test

--- tests/utils/encryptDecryptHandler.spec.js
+++ tests/utils/encryptDecryptHandler.spec.js
@@ -8,16 +8,22 @@ const {
 describe("encryptData function", () => {
   let note;
   let secretKey;
+  let encryptSpy;
+
   beforeEach(() => {
     note = "This is a secret note";
     secretKey = "MySecretKey";
+    encryptSpy = jest.spyOn(CryptoJS.AES, "encrypt");
   });
+
+  afterEach(() => {
+    encryptSpy.mockRestore();
+  });
+
   it("should call CryptoJS.AES.encrypt with expected params", () => {
-    const encryptSpy = jest.spyOn(CryptoJS.AES, "encrypt");
     encryptData(note, secretKey);
     expect(encryptSpy).toHaveBeenCalledTimes(1);
     expect(encryptSpy).toHaveBeenCalledWith(note, secretKey);
-    encryptSpy.mockRestore();
   });
 
   it("should encrypt the data if note and secret key is passed", () => {
@@ -30,7 +36,7 @@ describe("encryptData function", () => {
     secretKey = null;
     expect(() => {
       encryptData(note, secretKey);
-    }).toThrow(TypeError);
+    }).toThrow("Cannot read properties of null (reading 'words')");
   });
 });
 
@@ -38,18 +44,24 @@ describe("decryptData", () => {
   let note;
   let secretKey;
   let encryptedData;
+  let decryptSpy;
+
   beforeEach(() => {
     note = "This is a secret note";
     secretKey = "MySecretKey";
     encryptedData =
       "U2FsdGVkX19QuhHk+ZJC3ZnKVtGkwJhNRBLHZ+2YMy9B5sbWhM1lZ+d4+H0xbj1q";
+    decryptSpy = jest.spyOn(CryptoJS.AES, "decrypt");
   });
+
+  afterEach(() => {
+    decryptSpy.mockRestore();
+  });
+
   it("should call CryptoJS.AES.decrypt with expected params", () => {
-    const decryptSpy = jest.spyOn(CryptoJS.AES, "decrypt");
     decryptData(encryptedData, secretKey);
     expect(decryptSpy).toHaveBeenCalledTimes(1);
     expect(decryptSpy).toHaveBeenCalledWith(encryptedData, secretKey);
-    decryptSpy.mockRestore();
   });
 
   it("should decrypt the encrypted data", () => {
@@ -57,11 +69,12 @@ describe("decryptData", () => {
     expect(typeof decryptedData).toBe("string");
     expect(decryptedData).toBe(note);
   });
+
   it("should throw a TypeError if the secret key is null", () => {
     secretKey = null;
     expect(() => {
       decryptData(note, secretKey);
-    }).toThrow(TypeError);
+    }).toThrow("Cannot read properties of null (reading 'words')");
   });
 });
 
@@ -73,6 +86,5 @@ describe("hashData", () => {
       "af44ad1508d5592d9ce9a2524c184f594399f522a7e655a7c1b4ea5b76292cc094cdb8e411d968a02466a0775f415df094838275f0410cd7ca13f79b458e3787";
     const hashedData = hashData(note, secretKey);
     expect(hashedData).toBe(expectedHash);
-    expect(typeof hashedData).toBe("string");
   });
 });

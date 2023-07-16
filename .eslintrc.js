ff53de66b10813970418f475d0106c8684e12595
samresh9 <thapasamresh9@gmail.com>
2023-07-16 18:54:56 +0545

    temporary

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,10 @@ module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
   //extends: ["custom"],
+
+  env: {
+    es6: true,
+  },
   settings: {
     next: {
       rootDir: ["apps/*/"],

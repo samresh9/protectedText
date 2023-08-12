3450c39b0dd18380f114534b2aba0f4a07c5baaa
samresh9 <thapasamresh9@gmail.com>
2023-08-12 23:15:34 +0545

    PT-26: Move test and crypto-js dependency to shared-package

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,13 @@ module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
   //extends: ["custom"],
+  parserOptions: {
+        ecmaVersion: "latest"
+    },
+
+    env: {
+        es6: true
+    },
   settings: {
     next: {
       rootDir: ["apps/*/"],
--- packages/encrypt-handler/src/encryptDecryptHandler.js
+++ packages/encrypt-handler/src/encryptDecryptHandler.js
@@ -20,22 +20,3 @@ module.exports = {
   decryptData,
   hashData,
 };
-
-// import CryptoJS from "crypto-js";
-
-// export function encryptData(note, secretKey) {
-//   const cipherText = CryptoJS.AES.encrypt(note, secretKey).toString();
-//   return cipherText;
-// }
-
-// export function decryptData(encryptedData, secretKey) {
-//   const plainText = CryptoJS.AES.decrypt(encryptedData, secretKey).toString(
-//     CryptoJS.enc.Utf8
-//   );
-//   return plainText;
-// }
-
-// export function hashData(note, secretKey) {
-//   const hashText = CryptoJS.SHA512(note, secretKey).toString();
-//   return hashText;
-// }

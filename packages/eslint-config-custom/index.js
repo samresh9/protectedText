6dcdd820e08e4b8e7ce194b6856309ad08f4e93e
samresh9 <thapasamresh9@gmail.com>
2023-08-16 17:41:06 +0545

    PT-32:Status Check

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -11,30 +11,25 @@ module.exports = {
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
   ],
-  settings: {
-    react: {
-      version: "18.2.0",
-    },
-    rules: {
-      "prettier/prettier": [
-        "error",
-        {
-          endOfLine: "auto",
-        },
-      ],
-      "linebreak-style": ["error", "unix"],
-      "eol-last": ["warn", "always"],
-      "no-console": "error",
-      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-      "no-underscore-dangle": ["error", { allow: ["_id"] }],
-      quotes: [
-        "error",
-        "double",
-        { avoidEscape: true, allowTemplateLiterals: true },
-      ],
-    },
-    parserOptions: {
-      ecmaVersion: "latest",
-    },
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    "linebreak-style": ["error", "unix"],
+    "eol-last": ["warn", "always"],
+    "no-console": "error",
+    "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "no-underscore-dangle": ["error", { allow: ["_id"] }],
+    quotes: [
+      "error",
+      "double",
+      { avoidEscape: true, allowTemplateLiterals: true },
+    ],
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
   },
 };
